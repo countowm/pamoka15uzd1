7123ff57074cb78a1390f0d25b14778677c2f098
countowm <countowm@gmail.com>
2022-06-08 14:30:20 +0300

    pataisiau

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 window.onload = function(){
     let zodis = "mediena";
-    let raide = zodis[0];
+    let raide;
+    rezultatas = pirmaRaide(zodis[0]);
     console.log(raide);
+    let PirmaRaide = pirmaRaide(zodis, raide);
+    console.log(PirmaRaide);
+}
+function pirmaRaide(raide){
+    return raide[0];
 }
